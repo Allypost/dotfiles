48051685cbffa1955d293a4be162767d3c5690b8
Josip Igrec <me@allypost.net>
2021-07-02 01:31:16 +0200

    Move to GNU stow instead of managing symlinks manually

--- apply.sh
+++ apply.sh
@@ -4,107 +4,13 @@ SCRIPTPATH="$(
     cd "$(dirname "$0")" >/dev/null 2>&1
     pwd -P
 )"
-DOTFILES=\.*
 SCRIPTS_DIR="$HOME/.scripts"
 ANTIGEN_LOC="$HOME/.zsh-scripts/antigen.zsh"
 ASDF_LOC="$HOME/.asdf"
 EMACS_CONF_DIR="$HOME/.emacs.d"
-CONF_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
-DOOM_EMACS_CONF_DIR="$CONF_DIR/doom"
-ROFI_CONF_DIR="$CONF_DIR/rofi"
-XFCE4_CONF_DIR="$CONF_DIR/xfce4/"
-XFCE4_TERMINAL_CONF_DIR="$XFCE4_CONF_DIR/terminal"
-ARIA2_CONF_DIR="$CONF_DIR/aria2"
-MPV_CONF_DIR="$CONF_DIR/mpv"
-AWESOME_CONF_DIR="$CONF_DIR/awesome"
 
-echo "Adding symlinks to files..."
-for f in $DOTFILES; do
-    if [[ ! -f "$f" ]]; then
-        continue
-    fi
-
-    if [[ -L "$HOME/$f" ]]; then
-        echo "Removing old symlink to '$f'"
-        rm -i "$HOME/$f"
-    fi
-
-    if [[ -f "$HOME/$f" ]]; then
-        echo "Backing up: '$f' to '$f.bak'"
-        mv "$HOME/$f" "$HOME/$f.bak"
-    fi
-
-    ln -rs "$f" "$HOME/$f"
-done
-
-echo "Adding configs..."
-
-if [[ -d "$ROFI_CONF_DIR" ]]; then
-    echo "Rofi config already exists..."
-    printf "Do you want to override the existing config? [y/N] "
-    read OVERRIDE_ROFI_CONFIG
-
-    shopt -s nocasematch
-    if [[ $OVERRIDE_ROFI_CONFIG =~ (y|yes) ]]; then
-        mv "$ROFI_CONF_DIR" "$ROFI_CONF_DIR.bak"
-        ln -rs "$SCRIPTPATH/rofi/" "$ROFI_CONF_DIR"
-    fi
-    shopt -u nocasematch
-fi
-
-mkdir -p "$XFCE4_CONF_DIR"
-if [[ -d "$XFCE4_TERMINAL_CONF_DIR" ]]; then
-    echo "XFCE4 terminal config already exists..."
-    printf "Do you want to override the existing config? [y/N] "
-    read OVERRIDE_XFCE4_TERMINAL
-
-    shopt -s nocasematch
-    if [[ $OVERRIDE_XFCE4_TERMINAL =~ (y|yes) ]]; then
-        mv "$XFCE4_TERMINAL_CONF_DIR" "$XFCE4_TERMINAL_CONF_DIR.bak"
-    fi
-    shopt -u nocasematch
-fi
-ln -rs "$SCRIPTPATH/xfce4/terminal" "$XFCE4_CONF_DIR"
-
-mkdir -p "$ARIA2_CONF_DIR"
-if [[ -f "$ARIA2_CONF_DIR/aria2.conf" ]]; then
-    echo "Aria2 config already exists..."
-    printf "Do you want to override the existing config? [y/N]"
-    read OVERRIDE_ARIA2_CONFIG
-
-    shopt -s nocasematch
-    if [[ "$OVERRIDE_ARIA2_CONFIG" =~ (y|yes) ]]; then
-        mv "$ARIA2_CONF_DIR/aria2.conf" "$ARIA2_CONF_DIR/aria2.conf.bak"
-    fi
-    shopt -u nocasematch
-fi
-ln -rs "$SCRIPTPATH/aria2/aria2.conf" "$ARIA2_CONF_DIR"
-
-if [ -d "$AWESOME_CONF_DIR" ]; then
-    echo "awesome config already exists..."
-    printf "Do you want to override the existing config? [y/N]"
-    read OVERRIDE_AWESOME_CONFIG
-
-    shopt -s nocasematch
-    if [[ "$OVERRIDE_AWESOME_CONFIG" =~ (y|yes) ]]; then
-        mv "$AWESOME_CONF_DIR" "$AWESOME_CONF_DIR.bak"
-        ln -rs "$SCRIPTPATH/awesome" "$AWESOME_CONF_DIR"
-    fi
-    shopt -u nocasematch
-fi
-
-if [[ -d "$MPV_CONF_DIR" ]]; then
-    echo "mpv config already exists..."
-    printf "Do you want to override the existing config? [y/N]"
-    read OVERRIDE_MPV_CONFIG
-
-    shopt -s nocasematch
-    if [[ "$OVERRIDE_MPV_CONFIG" =~ (y|yes) ]]; then
-        mv "$MPV_CONF_DIR" "$MPV_CONF_DIR.bak"
-    fi
-    shopt -u nocasematch
-fi
-ln -rs "$SCRIPTPATH/mpv" "$MPV_CONF_DIR"
+# Run GNU stow to symlink everything
+stow */
 
 source "$HOME/.profile"
 
@@ -131,43 +37,6 @@ else
     cd "$ASDF_LOC"
     git checkout "$(git describe --abbrev=0 --tags)"
     cd -
-fi
-
-echo "Installing doom-emacs..."
-if [[ -d "$EMACS_CONF_DIR" ]]; then
-    echo ".emacs.d already exists..."
-    printf "Do you want to override the existing config? [y/N] "
-    read OVERRIDE_EMACS
-
-    shopt -s nocasematch
-    if [[ $OVERRIDE_EMACS =~ (y|yes) ]]; then
-        EMACS__INSTALL_DOOM_EMACS=1
-    fi
-    shopt -u nocasematch
-else
-    EMACS__INSTALL_DOOM_EMACS=1
-fi
-if [ ! -z "$EMACS__INSTALL_DOOM_EMACS" ]; then
-    rm -rf "$EMACS_CONF_DIR"
-    git clone --depth 1 https://github.com/hlissner/doom-emacs "$EMACS_CONF_DIR"
-fi
-
-if [ -d "$DOOM_EMACS_CONF_DIR" ]; then
-    echo "doom-emacs config already exists..."
-    printf "Do you want to override the existing config? [y/N] "
-    read OVERRIDE_DOOM_EMACS
-
-    shopt -s nocasematch
-    if [[ $OVERRIDE_DOOM_EMACS =~ (y|yes) ]]; then
-        EMACS__INSTALL_DOOM_EMACS_CONFIG=1
-    fi
-    shopt -u nocasematch
-else
-    EMACS__INSTALL_DOOM_EMACS_CONFIG=1
-fi
-if [ ! -z "$EMACS__INSTALL_DOOM_EMACS_CONFIG" ]; then
-    rm -rf "$DOOM_EMACS_CONF_DIR"
-    ln -rs "$SCRIPTPATH/doom-emacs-config" "$DOOM_EMACS_CONF_DIR"
 fi
 
 # Remove completions for some programs
