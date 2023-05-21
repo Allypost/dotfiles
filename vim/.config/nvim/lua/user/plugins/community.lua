9893bc3f327eb72db1eec287ff7060a528410f19
Josip Igrec <josip@igr.ec>
2023-05-21 18:22:46 +0200

    Format community plugins file

--- vim/.config/nvim/lua/user/plugins/community.lua
+++ vim/.config/nvim/lua/user/plugins/community.lua
@@ -1,28 +1,30 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
+	-- Add the community repository of plugin specifications
+	"AstroNvim/astrocommunity",
 
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.prisma" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.python" },
+	-- pack
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.pack.vue" },
+	{ import = "astrocommunity.pack.prisma" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+	{ import = "astrocommunity.pack.yaml" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.bash" },
+	{ import = "astrocommunity.pack.python" },
 
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
-  {
-    "catppuccin",
-    opts = {
-      flavor = "mocha",
-    },
-  },
-  { import = "astrocommunity.colorscheme.nightfox", enabled = false },
-  { import = "astrocommunity.colorscheme.oxocarbon", enabled = false },
-  { import = "astrocommunity.colorscheme.tokyonight", enabled = false },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+	-- colorscheme
+	{ import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+	{
+		"catppuccin",
+		opts = {
+			flavor = "mocha",
+		},
+	},
+	{ import = "astrocommunity.colorscheme.nightfox",      enabled = false },
+	{ import = "astrocommunity.colorscheme.oxocarbon",     enabled = false },
+	{ import = "astrocommunity.colorscheme.tokyonight",    enabled = false },
+
+	-- completion
+	{ import = "astrocommunity.completion.copilot-lua-cmp" },
 }
