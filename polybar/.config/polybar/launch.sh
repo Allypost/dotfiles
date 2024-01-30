29a408dbc43080de9b72a50358e7433c0054eefa
Josip Igrec <josip@igr.ec>
2024-01-31 00:03:53 +0100

    Update minor polybar stuff

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,17 +2,19 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 while pgrep -u $UID -x polybar >/dev/null; do
-  sleep 0.5
+	sleep 0.5
 done
 
 if type "xrandr"; then
-  for m in $(xrandr --query | grep ' connected' | cut -d' ' -f1); do
-    MONITOR=$m polybar --reload example 2>&1 | tee -a "/tmp/polybar-$m.log" & disown
-  done
-else 
-  polybar --reload example 2>&1 | tee -a "/tmp/polybar-monitor-main.log" & disown
+	for m in $(xrandr --query | grep ' connected' | cut -d' ' -f1); do
+		MONITOR=$m polybar --reload example 2>&1 | tee -a "/tmp/polybar-$m.log" &
+		disown
+	done
+else
+	polybar --reload example 2>&1 | tee -a "/tmp/polybar-monitor-main.log" &
+	disown
 fi
