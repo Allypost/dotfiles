0d88fb8bd093d9437327d9d1cd8f06df2d7e20ae
Josip Igrec <josip@igr.ec>
2024-05-11 15:58:44 +0200

    Add `suda` and `range-highlight` nvim plugins

---
+++ vim/.config/nvim/lua/plugins/range-highlight.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "winston0410/range-highlight.nvim",
+    dependencies = {
+      "winston0410/cmd-parser.nvim",
+    },
+  },
+}
---
+++ vim/.config/nvim/lua/plugins/suda.lua
@@ -0,0 +1,8 @@
+return {
+  "lambdalisue/suda.vim",
+  lazy = true,
+  cmd = {
+    "SudaRead",
+    "SudaWrite",
+  },
+}
