205e874dbdc5303aac0cc2048f8570324e91e92b
Josip Igrec <josip@igr.ec>
2024-06-14 12:17:03 +0200

    Fix using deprecated lazyvim config

--- vim/.config/nvim/lua/plugins/telescope.lua
+++ vim/.config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     "nvim-telescope/telescope.nvim",
     keys = {
       { "<leader>/", false },
-      { "<leader>fl", Util.telescope("live_grep"), desc = "Grep (root dir)" },
+      { "<leader>fl", Util.pick("live_grep"), desc = "Grep (Root Dir)" },
     },
   },
 }
