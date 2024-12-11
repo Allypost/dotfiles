9e72fa78d93b1de30f86184d70cfcb48a84a2340
Josip Igrec <josip@igr.ec>
2024-12-11 14:51:04 +0100

    Update nvim config

--- vim/.config/nvim/lua/config/options.lua
+++ vim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,7 @@
 -- Add any additional options here
 
 vim.g.nofoldenable = true
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
